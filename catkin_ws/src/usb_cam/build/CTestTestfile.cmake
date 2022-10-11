86da14704e9b62369b6283f2559878bf64d033d3
branyang02 <brandonbrid2100@gmail.com>
2022-10-11 15:12:44 -0400

    test

---
+++ catkin_ws/src/usb_cam/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/akashs/apriltag_detection/catkin_ws/src/usb_cam
+# Build directory: /home/akashs/apriltag_detection/catkin_ws/src/usb_cam/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
